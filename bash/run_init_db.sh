d028d4083f00b8b31e2f2fe7804c83feef399fbc
EvaIvina <46657597+EvaIvina@users.noreply.github.com>
2023-06-23 20:41:55 +0300

    Update run_init_db.sh

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+CONTAINER_NAME="postgres_demo_db"
+
+docker pull postgres
+
+docker run --name $CONTAINER_NAME --rm -p 5432:5432 \
+  -e POSTGRES_USER=test_sde \
+  -e POSTGRES_PASSWORD=@sde_password012 \
+  -e POSTGRES_DB=demo \
+  -e PGDATA=/var/lib/postgresql/data/pgdata \
+  -v $HOME/sde_test_db/sql:/var/lib/postgresql/data \
+  -d postgres
+
+sleep 10
+
+docker exec $CONTAINER_NAME psql -U test_sde \
+  -d demo -f /var/lib/postgresql/data/init_db/demo.sql
